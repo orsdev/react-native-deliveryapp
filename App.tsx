58a65dfeda792333d884dfa8f40c2ffc35fb051c
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-09-11 12:28:18 +0100

    🔥 some useless code removed

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 // import { store } from './redux/store'
 import { Provider } from 'react-redux'
-import { Text } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'react-native-elements';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -39,9 +37,7 @@ export default function App() {
   return (
     // <Provider store={store}>
     <ThemeProvider>
-      <SafeAreaProvider>
-        <Navigations />
-      </SafeAreaProvider>
+      <Navigations />
     </ThemeProvider>
     // </Provider>
   );
